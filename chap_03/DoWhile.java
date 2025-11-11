cfd41755c8daaf43a9ac084554685965295eb557
md.asifkhan <20240659130asif@juniv.edu>
2025-11-11 12:50:18 +0600

    ll

---
+++ chap_03/DoWhile.java
@@ -0,0 +1,14 @@
+class DoWhile
+{
+	public static void main( String[] args )
+	{	
+		int num = 10 ;
+
+		do
+		{
+			System.out.println( "DoWhile Countup: " + num );	
+			num += 10;
+		}
+		while ( num <= 100 );		
+	}
+}
